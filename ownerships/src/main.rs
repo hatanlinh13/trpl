3c67e17bd23f9b3c887b4e05208aa0f7a1849ce9
hatanlinh13 <hatanlinh13@gmail.com>
2021-12-10 20:26:27 +0700

    Add code for chapter 4: Ownership

---
+++ ownerships/src/main.rs
@@ -0,0 +1,19 @@
+fn main()
+{
+    let string = String::from("Hello, world!");
+    let first = first_word(&string);
+    println!("The first word is: {}", first);
+}
+
+fn first_word(s: &str) -> &str
+{
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
