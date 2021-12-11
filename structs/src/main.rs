2a6d034248ecc95c01ea365b5cc40393e64ffd00
hatanlinh13 <hatanlinh13@gmail.com>
2021-12-11 11:37:43 +0700

    Add code for chapter 5

---
+++ structs/src/main.rs
@@ -0,0 +1,38 @@
+#[derive(Debug)]
+struct User
+{
+    activate: bool,
+    name: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, name: String) -> User
+{
+    User { email,
+           name,
+           activate: true,
+           sign_in_count: 1 }
+}
+
+fn main()
+{
+    let user1 = build_user(String::from("user1@domain"), String::from("user"));
+    println!("rect1 - activate: {} - name: {} - email: {} - sign in count: {}",
+             user1.activate, user1.name, user1.email, user1.sign_in_count);
+
+    let user2 = User { email: String::from("user2@domain"),
+                       ..user1 };
+    dbg!(&user2);
+
+    // tuple structs
+    #[derive(Debug)]
+    struct Color(i32, i32, i32);
+    #[derive(Debug)]
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    dbg!(&black);
+    dbg!(&origin);
+}
