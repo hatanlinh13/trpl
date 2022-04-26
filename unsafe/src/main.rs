4bab584196d2ff6d649a5efe661deb16b185dac0
Ha Tan Linh <hatanlinh13@gmail.com>
2022-04-26 22:39:27 +0700

    Add code for chapter 19

---
+++ unsafe/src/main.rs
@@ -0,0 +1,58 @@
+use std::slice;
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
+
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+fn main() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    let mut vector = vec![1, 2, 3, 4, 5, 6];
+    let (left, right) = split_at_mut(&mut vector, 3);
+    println!("left {:?}", left);
+    println!("right {:?}", right);
+
+    unsafe {
+        println!("Absolute value of -5 according to C: {}", abs(-5));
+    }
+
+    add_to_count(10);
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
+}
